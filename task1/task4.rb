3a8fb50003cec03dbb6396f02eb210daf36df011
Kagayakashi <kagayakashi.vo@gmail.com>
2020-08-25 17:55:01 +0000

    Lesson 1 update

--- task1/task4.rb
+++ task1/task4.rb
@@ -4,16 +4,17 @@ b = gets.chomp.to_f
 c = gets.chomp.to_f
 
 d = b * b - 4 * a * c
-x1 = (-b + c) / (2 * a)
-x2 = (-b - c) / (2 * a)
+
+a2 = 2 * a
 
 if d > 0
-  x1 = (-b - Math.sqrt(d)) / (2 * a)
-  x2 = (-b + Math.sqrt(d)) / (2 * a)
+  d_sqrt = Math.sqrt(d)
+  x1 = (-b - d_sqrt) / a2
+  x2 = (-b + d_sqrt) / a2
   puts "Дискриминант #{d}. Первый корень #{x1}, второй корень #{x2}"
 elsif d < 0
   puts "Дискриминант #{d}. Корней нет."
 else
-  x1 = -b / (2 * a)
+  x1 = -b / a2
   puts "Дискриминант #{d}. Корень #{x1}"
 end
