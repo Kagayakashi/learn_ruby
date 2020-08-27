a5a576c15f389a3bbcb26614e2b0c7befadff25d
Kagayakashi <kagayakashi.vo@gmail.com>
2020-08-27 03:42:04 +0000

    Task 2 update

--- task2/task5.rb
+++ task2/task5.rb
@@ -1,1 +1,21 @@
-puts "Введи"
+puts "Пожалуйста, введите дату в числовом формате."
+
+puts "Год:"
+input_year = gets.chomp.to_i
+
+puts "Месяц:"
+input_month = gets.chomp.to_i
+
+puts "День:"
+input_day = gets.chomp.to_i
+
+# Посчитать количество дней используя месяц
+days_in_months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+result = 0
+
+i = 0
+while i < input_month
+  result += days_in_months[i]
+end
+
+puts result
