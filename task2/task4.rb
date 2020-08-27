300f5f36090f0a7957a9b70b82d7a4c9949fdbc5
Kagayakashi <kagayakashi.vo@gmail.com>
2020-08-27 06:34:31 +0000

    Task2(4) update. Added vowels

--- task2/task4.rb
+++ task2/task4.rb
@@ -1,11 +1,15 @@
 puts "Хеш латинских букв и их порядковый номер"
 
 letters = ("A".."Z").to_a
+vowels = ["A","E","I","O","U","Y"]
 letter_hash = Hash.new
 
 # Заполнить хеш буквами и порядковым номером
 letters.count.times do |order|
-  letter_hash[letters[order]] = order + 1
+  letter = letters[order]
+  if vowels.include?(letter)
+    letter_hash[letter] = order + 1
+  end
 end
 
 # Вывести хеш букв
