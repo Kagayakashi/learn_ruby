d9e1f43ef1c7f6d69a8d0cb8f91f66d138d5dad2
Kagayakashi <kagayakashi.vo@gmail.com>
2020-08-29 11:12:42 +0000

    Second Optimization lesson 2

--- lesson2/task5.rb
+++ lesson2/task5.rb
@@ -10,18 +10,12 @@ puts "День:"
 input_day = gets.chomp.to_i
 
 # Весокосный год или нет
-leap_year = input_year % 400 == 0 || input_year % 4 == 0 ? true : false
+leap_year = input_year % 4 == 0 && input_year % 100 != 0 || input_year % 400 == 0
 
 # Посчитать количество дней используя месяц
 days_in_months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-days_count = input_day
 
-i = 1
-while i < input_month
-  days_count += days_in_months[i-1]
-  i += 1
-end
-
+days_count = input_day + days_in_months.take(input_month - 1).sum
 days_count -= 1 if !leap_year
 
 puts days_count
