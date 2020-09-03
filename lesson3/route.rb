f60084e6a546fc7086955231bc6cd3518add6199
Kagayakashi <kagayakashi.vo@gmail.com>
2020-09-03 13:31:25 +0000

    Updated lists from hash to array. Arrays save objects

--- lesson3/route.rb
+++ lesson3/route.rb
@@ -1,71 +1,30 @@
 class Route
 
-  attr_reader :stations, :max_stations
+  attr_reader :stations
 
-  def initialize(route_start, route_end)
-    @route_start = route_start
-    @route_end = route_end
-    
-    @max_stations = 3
-    
-    @stations = {
-      route_start => 1,
-      route_end => @max_stations,
-    }
-    
+  def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
+
     welcome
   end
   
   def welcome
-    puts "Маршрут от #{@route_start} до #{@route_end} создан!"
+    puts "Маршрут от #{@stations[0].name} до #{@stations[1].name} создан!"
   end
   
-  def first_station
-    @stations.keys[0]
+  def add(station)
+    @stations.insert(@stations.length - 1, station)
+    puts "\nВ маршрут добавлена станция #{station.name}."
   end
   
-  def add(name, order)
-    if @stations.has_key?(name)
-      puts "\nНазвание станции уже занято. Станция не добавлена."
-      return
-    elsif @stations.has_value?(order)
-      puts "\nПорядковый номер станции уже занят. Станция не добавлена."
-      return
-    end
-    
-    @stations[name] = order
-    puts "\nВ маршрут добавлена станция #{name}."
-  end
-  
-  def del(name)
-    if @stations[name] == 1 || @stations[name] == @max_stations
-      puts "Нельзя удалить первоначальные станции"
-      return
-    end
-    
-    @stations.delete(name)
-    puts "\nИз маршрута удалена станция #{name}."
+  def del(station)
+    @stations.delete(station)
+    puts "\nИз маршрута удалена станция #{station.name}."
   end
   
   def list
     puts "\nСписок станций:\n"
-    filter = proc {|_name, order| order}
-    #@stations.sort_by {|_name, order| order}.each {|name, _order| puts name}
-    @stations.sort_by(&filter).each {|name, _order| puts name}
+    @stations.each {|station| puts station.name}
   end
 
 end
-
-path = Route.new("Париж", "Берлин")
-
-path.add("Барселона", 2)
-path.add("Барселона", 2) # промежуточная станция не добавлена
-path.add("Рига", 1)      # промежуточная станция не добавлена
-
-path.add("Рига", 3)
-
-path.list # Список всего маршрута
-
-path.del("Барселона") # Удалить станцию
-
-path.list
--- lesson3/train.rb
+++ lesson3/train.rb
@@ -1,8 +1,5 @@
-require_relative 'route'
-
 class Train
-  
-  attr_reader :speed, :car_amount
+  attr_reader :speed, :car_amount, :num, :type, :car_amount
   attr_writer :route
 
   def initialize(num, type, car_amount)
@@ -62,13 +59,11 @@ class Train
   
   def route=(route)
     @route = route
-    @station_number = 1
-    
-    update_station
+    @station_number = 0
   end
   
-  def next_station
-    if @station_number == @route.max_stations
+  def goto_next_station
+    if @station_number == @route.stations.length
       puts "Мы находимся на последней станции!"
       return
     elsif @speed == 0
@@ -77,11 +72,10 @@ class Train
     end
     
     @station_number += 1
-    update_station
   end
   
-  def prev_station
-    if @station_number == 1
+  def goto_prev_station
+    if @station_number == 0
       puts "Мы находимся на первой станции!"
       return
     elsif @speed == 0
@@ -90,88 +84,24 @@ class Train
     end
   
     @station_number -= 1
-    update_station
   end
   
-  def update_station
-    @current_station = @route.stations.key(@station_number)
-    @next_station = @route.stations.key(@station_number + 1)
-    @prev_station = @route.stations.key(@station_number - 1)
+  def current_station
+    @route.stations[@station_number]
+  end
+  
+  def next_station
+    @route.stations[@station_number + 1]
+  end
+  
+  def prev_station
+    @route.stations[@station_number - 1] if @station_number - 1 != -1
   end
   
   def stations
-    puts "Предыдущая станция: #{@prev_station == nil ? "~" : @prev_station}.\nТекущая станция: #{@current_station}.\nСледующая станция: #{@next_station == nil ? "~" : @next_station}."
+    puts "Предыдущая станция: #{prev_station.name}." if prev_station
+    puts "Текущая станция: #{current_station.name}."
+    puts "Следующая станция: #{next_station.name}." if next_station
   end
   
 end
-
-train = Train.new("Вулкан", "Пассажирский", 8)
-
-route = Route.new("Париж", "Берлин")
-
-route.add("Барселона", 2)
-route.add("Рига", 3)
-train.route = route
-
-puts train.speed
-puts train.car_amount
-
-train.speed_up # 1
-train.speed_up # 2
-train.speed_up # 3
-train.speed_up # 4
-train.speed_up # 5
-train.speed_up # 6
-train.speed_up # 7
-train.speed_up # 8
-train.speed_up # 9
-train.speed_up # 10
-train.speed_up # max
-train.speed_up # max
-
-puts train.speed
-
-train.speed_down # 9
-train.speed_down # 8
-
-puts train.speed
-
-train.speed_down # 7
-train.speed_down # 6
-train.speed_down # 5
-train.speed_down # 4
-train.speed_down # 3
-train.speed_down # 2
-
-train.add_car
-train.del_car
-
-train.speed_down # 1
-train.speed_down # stop
-train.speed_down # stop
-
-puts train.speed
-
-train.add_car
-train.del_car
-
-puts train.car_amount
-
-train.next_station
-train.next_station
-
-train.stations
-
-train.speed_up # 1
-
-train.next_station
-train.next_station
-train.next_station
-
-train.stations
-
-train.prev_station
-train.prev_station
-train.prev_station
-
-train.stations
