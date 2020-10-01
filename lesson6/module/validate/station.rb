e7e68f22d56289bb97f03851619554d17e2dc9f3
Kagayakashi <kagayakashi.vo@gmail.com>
2020-10-01 06:59:44 +0000

    Added error type at 'raise'

--- lesson6/module/validate/station.rb
+++ lesson6/module/validate/station.rb
@@ -1,6 +1,6 @@
 module StationValidator
   private
   def validate!
-    raise if name.length < 3 || name.length > 20
+    raise RuntimeError if name.length < 3 || name.length > 20
   end
 end
--- lesson6/module/validate/train.rb
+++ lesson6/module/validate/train.rb
@@ -2,6 +2,6 @@ module TrainValidator
   REXP_ALLOWED = /^[A-Za-z]{2}-[0-9]{4}$/i
   private
   def validate!
-    raise if num !~ REXP_ALLOWED
+    raise RuntimeError if num !~ REXP_ALLOWED
   end
 end
